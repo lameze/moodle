f35199bca2bd81292b1d5fe6cafc77483df757fc
Ankit Agarwal <ankit@moodle.com>
2014-05-19 14:10:29 +0800

    Add basic report structure

---
+++ report/trigger/index.php
@@ -0,0 +1,60 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file gives an overview of the triggers present in site.
+ *
+ * @package    report_trigger
+ * @copyright  2014 onwards Ankit Agarwal <ankit.agrr@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require('../../config.php');
+require_once($CFG->libdir.'/adminlib.php');
+require_once($CFG->dirroot.'/course/lib.php');
+
+$id = optional_param('id', 0, PARAM_INT);
+
+if (empty($id)) {
+    require_login();
+    $context = context_system::instance();
+    $coursename = format_string($SITE->fullname, true, array('context' => $context));
+} else {
+    $course = get_course($id);
+    require_login($course);
+    $context = context_course::instance($course->id);
+    $coursename = format_string($course->fullname, true, array('context' => $context));
+}
+require_capability('report/trigger:view', $context);
+
+$url = new moodle_url("/report/trigger/index.php", array('id' => $id));
+$a = new stdClass();
+$a->coursename = $coursename;
+$a->reportname = get_string('pluginname', 'report_trigger');
+$title = get_string('title', 'report_trigger', $a);
+
+$PAGE->set_url($url);
+$PAGE->set_pagelayout('report');
+$PAGE->set_context($context);
+$PAGE->set_title($title);
+$PAGE->set_heading($title);
+
+if (empty($id)) {
+    admin_externalpage_setup('reporttrigger', '', null, '', array('pagelayout' => 'report'));
+}
+
+echo $OUTPUT->header();
+echo $OUTPUT->footer();
---
+++ report/trigger/lib.php
@@ -0,0 +1,56 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Libs, public API.
+ *
+ * @package    report_trigger
+ * @copyright  2014 onwards Ankit Agarwal <ankit.agrr@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+/**
+ * This function extends the navigation with the report items
+ *
+ * @global stdClass $CFG
+ * @global core_renderer $OUTPUT
+ * @param navigation_node $navigation The navigation node to extend
+ * @param stdClass        $course     The course to object for the report
+ * @param context         $context    The context of the course
+ */
+function report_trigger_extend_navigation_course($navigation, $course, $context) {
+    if (has_capability('report/trigger:view', $context)) {
+        $url = new moodle_url('/report/trigger/index.php', array('id' => $course->id));
+        $navigation->add(get_string('pluginname', 'report_trigger'), $url, navigation_node::TYPE_SETTING, null, null,
+                new pix_icon('i/report', ''));
+    }
+}
+
+/**
+ * Callback to verify if the given instance of store is supported by this report or not.
+ *
+ * @param string $instance store instance.
+ *
+ * @return bool returns true if the store is supported by the report, false otherwise.
+ */
+function report_trigger_supports_logstore($instance) {
+    if ($instance instanceof \core\log\sql_select_reader) {
+        return true;
+    }
+    return false;
+}
