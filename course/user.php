696466ee3eaf3769fe0b4524170929d904e58d97
Simey Lameze <simey@moodle.com>
2015-04-17 10:57:39 +0800

    MDL-49854 core: fix issues on navigation code

--- course/user.php
+++ course/user.php
@@ -175,8 +175,8 @@ switch ($mode) {
 
         break;
     default:
-        // can not be reached ;-)
-        // But just incase let's not break the navigation.
+        // It's unlikely to reach this piece of code, as the mode is never empty and it sets mode as grade in last case.
+        // Display the page header to avoid breaking the navigation.
         echo $OUTPUT->header();
 }
 
