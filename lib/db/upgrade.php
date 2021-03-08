794ac989eeb6b70a34ab06e771bf46d52bb54273
Simey Lameze <simey@moodle.com>
2021-03-09 06:42:09 +0800

    MDL-70781 course: setting for displaying completion conditions
    Part of MDL-70817

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -2509,5 +2509,19 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2021031200.01);
     }
 
+    if ($oldversion < 2021031200.02) {
+        // Define field 'showcompletionconditions' to be added to course.
+        $table = new xmldb_table('course');
+        $field = new xmldb_field('showcompletionconditions', XMLDB_TYPE_INTEGER, '1', null,
+            XMLDB_NOTNULL, null, '1', 'completionnotify');
+
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Main savepoint reached.
+        upgrade_main_savepoint(true, 2021031200.02);
+    }
+
     return true;
 }
