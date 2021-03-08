3a1b2cb293dbdccc1ed39a9d4fb9555913c1856e
Simey Lameze <simey@moodle.com>
2021-03-08 20:46:52 +0800

    MDL-70782 course: add setting to show activity dates
    Part of MDL-70817

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -2495,5 +2495,19 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2021030500.01);
     }
 
+    if ($oldversion < 2021031200.01) {
+        // Define field 'showactivitydates' to be added to course table.
+        $table = new xmldb_table('course');
+        $field = new xmldb_field('showactivitydates', XMLDB_TYPE_INTEGER, '1', null,
+            XMLDB_NOTNULL, null, '0', 'originalcourseid');
+
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Main savepoint reached.
+        upgrade_main_savepoint(true, 2021031200.01);
+    }
+
     return true;
 }
