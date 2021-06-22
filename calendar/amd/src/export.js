d9b13ed4f54aab1265b8fa0e02aa5d88c3a7d48b
Jun Pataleta <jun@moodle.com>
2021-06-22 14:09:38 +0800

    MDL-71788 calendar: Make it easier to copy the calendar URL
    Converted the Calendar URL string to a read-only text input containing
    the calendar URL. Pressing the "Copy URL" button will copy the calendar
    URL to the clipboard.
    Fixed Behat tests as well for the export feature. With the calendar URL
    in a text input that clips the whole URL, the `preset_what` URL
    parameter cannot be seen by the "I should see" step. Worked around
    this by checking that the actual value attribute of the calendar URL
    text input contains the preset_what parameter.
    
    Plus: Deprecate the calendarurl string. It will be replaced by a new
    lang string that does not contain the actual calendar URL.

---
+++ calendar/amd/src/export.js
@@ -0,0 +1,57 @@
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * A javascript module to enhance the calendar export form.
+ *
+ * @module     core_calendar/export
+ * @copyright  2021 Jun Pataleta
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+import {enhance as copyToClipboardEnhance} from 'core/copy_to_clipboard';
+
+/**
+ * Selectors for the calendar export page.
+ *
+ * @property {string} copyUrlId The element ID of the Copy URL button.
+ * @property {string} exportUrlId The element ID of the Calendar URL text field.
+ */
+const selectors = {
+    copyUrlId: 'copyexporturl',
+    exportUrlId: 'calendarexporturl',
+};
+
+/**
+ * Initialises the calendar export JS module.
+ *
+ * @method init
+ */
+export const init = () => {
+    // Enable the copy URL button and focus on it after the page has loaded.
+    document.onreadystatechange = () => {
+        if (document.readyState === 'complete') {
+            const copyUrl = document.getElementById(selectors.copyUrlId);
+            copyUrl.removeAttribute('disabled');
+            copyUrl.focus();
+        }
+    };
+
+    // Initialise copy to clipboard functionality.
+    copyToClipboardEnhance(selectors.copyUrlId, selectors.exportUrlId, {
+        messageKey: 'calendarurlcopiedtoclipboard',
+        messageComponent: 'calendar'
+    });
+};
