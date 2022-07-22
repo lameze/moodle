9ae64bc609721f43d35f1b8ea661f1a95d25debc
Simey Lameze <simey@moodle.com>
2022-07-22 09:06:08 +0800

    MDL-75282 editor_tiny: make tiny default editor

--- lib/editor/tiny/db/install.php
+++ lib/editor/tiny/db/install.php
@@ -15,36 +15,36 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Atto upgrade script.
+ * Tiny upgrade script.
  *
- * @package    editor_atto
- * @copyright  2014 Damyon Wiese
+ * @package    editor_tiny
+ * @copyright  2022 Simey Lameze
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
 /**
- * Make the Atto the default editor for upgrades from 26.
+ * Make the Tiny the default editor.
  *
  * @return bool
  */
-function xmldb_editor_atto_install() {
+function xmldb_editor_tiny_install() {
     global $CFG;
 
-//    // Make Atto the default.
-//    $currenteditors = $CFG->texteditors;
-//    $neweditors = array();
-//
-//    $list = explode(',', $currenteditors);
-//    array_push($neweditors, 'atto');
-//    foreach ($list as $editor) {
-//        if ($editor != 'atto') {
-//            array_push($neweditors, $editor);
-//        }
-//    }
-//
-//    set_config('texteditors', implode(',', $neweditors));
+    // Make Tiny the default editor.
+    $currenteditors = $CFG->texteditors;
+    $neweditors = [];
+
+    $list = explode(',', $currenteditors);
+    array_push($neweditors, 'tiny');
+    foreach ($list as $editor) {
+        if ($editor != 'tiny') {
+            array_push($neweditors, $editor);
+        }
+    }
+
+    set_config('texteditors', implode(',', $neweditors));
 
     return true;
 }
