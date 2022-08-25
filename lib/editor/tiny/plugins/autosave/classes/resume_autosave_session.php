8593b3f5f4deabab057c2420c2ebf467cbd09953
Andrew Nicols <andrew@nicols.co.uk>
2022-08-25 23:48:30 +0800

    ALPHANUMEXT is not sufficient

--- lib/editor/tiny/plugins/autosave/classes/resume_autosave_session.php
+++ lib/editor/tiny/plugins/autosave/classes/resume_autosave_session.php
@@ -44,7 +44,7 @@ class resume_autosave_session extends external_api {
             'contextid' => new external_value(PARAM_INT, 'The context id that owns the editor', VALUE_REQUIRED),
             'pagehash' => new external_value(PARAM_ALPHANUMEXT, 'The page hash', VALUE_REQUIRED),
             'pageinstance' => new external_value(PARAM_ALPHANUMEXT, 'The page instance', VALUE_REQUIRED),
-            'elementid' => new external_value(PARAM_ALPHANUMEXT, 'The ID of the element', VALUE_REQUIRED),
+            'elementid' => new external_value(PARAM_RAW, 'The ID of the element', VALUE_REQUIRED),
             'draftid' => new external_value(
                 PARAM_INT,
                 'The new draft item id to resume files to',
