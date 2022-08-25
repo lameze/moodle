a631d71af6f7b971881f610baaf3db5c79b8b2af
Andrew Nicols <andrew@nicols.co.uk>
2022-08-25 22:27:01 +0800

    Move behat editor value setting to editor plugins

---
+++ lib/editor/textarea/tests/behat/behat_editor_textarea.php
@@ -0,0 +1,49 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Moodle textarea editor field.
+ *
+ * @package    core_form
+ * @category   test
+ * @copyright  2022 Andrew Lyons <andrew@nicols.co.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(__DIR__ . '/../../../../behat/behat_base.php');
+
+class behat_editor_textarea extends behat_base {
+
+    /**
+     * Set the value for the editor.
+     *
+     * @param $params
+     * @param $editorid
+     * @param $value
+     * @return void
+     */
+    public function set_editor_value($params, $editorid, $value) {
+        $js = <<<EOF
+            (function() {
+                const editor = document.getElementById("${editorid}");
+                if (editor && editor.tagName.toLowerCase() === 'textarea') {
+                    editor.value = "${value}";
+                }
+            })();
+        EOF;
+        behat_base::execute_script_in_session($params->getSession(), $js);
+    }
+}
--- lib/editor/tiny/tests/behat/behat_editor_tiny.php
+++ lib/editor/tiny/tests/behat/behat_editor_tiny.php
@@ -11,14 +11,13 @@ class behat_editor_tiny extends behat_base {
      * @return void
      */
     public function set_editor_value($params, $editorid, $value) {
-
-$js = "
-require(['editor_tiny/editor'], (editor) => {
-    const instance = editor.getInstanceForElementId('${editorid}');
-    instance.setContent('${value}');
-    instance.undoManager.add();
-});
-";
+        $js = <<<EOF
+            require(['editor_tiny/editor'], (editor) => {
+                const instance = editor.getInstanceForElementId('${editorid}');
+                instance.setContent('${value}');
+                instance.undoManager.add();
+            });
+        EOF;
         behat_base::execute_script_in_session($params->getSession(), $js);
     }
 }
