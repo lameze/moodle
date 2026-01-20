fdc658f70fb346f87374850076485aa069533c17
Simey Lameze <simey@moodle.com>
2026-01-20 10:19:15 +0800

    sleep for 5 seconds

--- public/login/index.php
+++ public/login/index.php
@@ -26,7 +26,7 @@
 
 require('../config.php');
 require_once('lib.php');
-
+sleep(5);
 redirect_if_major_upgrade_required();
 
 $testsession = optional_param('testsession', 0, PARAM_INT); // test session works properly
