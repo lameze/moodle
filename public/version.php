c005ccc49b4829d4d3827404b281f7036ae132fc
Simey Lameze <simey@moodle.com>
2026-03-05 11:19:23 +0800

    no regression

--- public/version.php
+++ public/version.php
@@ -26,7 +26,7 @@
  * @copyright  1999 onwards Martin Dougiamas (http://dougiamas.com)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
+# just a comment
 defined('MOODLE_INTERNAL') || die();
 
 $version  = 2026022700.00;              // YYYYMMDD      = weekly release date of this DEV branch.
